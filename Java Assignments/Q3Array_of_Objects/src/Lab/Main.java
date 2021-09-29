bee542731b338fe68a2797a08d57f4bfc3bb0166
Gaurav_Shinde <shindegaurav013@gmail.com>
2021-09-29 19:55:32 +0530

    data members and constructor created

---
+++ Java Assignments/Q3Array_of_Objects/src/Lab/Main.java
@@ -0,0 +1,23 @@
+package Lab;
+
+import java.util.Scanner;
+
+class Employee{
+    private String name;
+    private float salary;
+    private String str[] = {"year", "month", "day"};
+    String DOJ = "";
+
+
+    Employee() {
+        this.name = name;
+        this.salary = salary;
+    }
+
+}
+public class Main {
+
+    public static void main(String[] args) {
+	
+    }
+}
